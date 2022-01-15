1a259e70dd82a1a2a84bcec01ac6fadfcc454142
nanggi4 <nkhun1006@gmail.com>
2022-01-15 10:51:02 +0900

    while 2 lecture

--- loops/temp2.py
+++ loops/temp2.py
@@ -1,4 +1,14 @@
-while condition:
-    Action
-    Action2
-    Action3
+# while condition:
+#    Action
+#    Action2
+#    Action3
+
+counter = 1
+Sum = 0
+
+while counter <= 10:
+    # print(counter)
+    Sum += counter
+    counter = counter + 1
+
+print(Sum)
