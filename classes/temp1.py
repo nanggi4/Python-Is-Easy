c9b45f89ebb8626d021b5869af3ff7ae3170c40c
nanggi4 <nkhun1006@gmail.com>
2022-02-19 23:16:07 +0900

    class lecture 2

--- classes/temp1.py
+++ classes/temp1.py
@@ -1,3 +1,25 @@
-class ClassName:
-  def __init__(self.)
-  self
+#class ClassName:
+#  def __init__(self):
+#    self.Attribute = 0
+#  
+#  def AnotherFunction(self):
+#    Action(s)
+
+class Team:
+  def __init__(self, Name, Origin):
+    self.TeamName = Name
+    self.TeamOrigin = Origin
+  
+  def DefineTeamName(self, Name):
+    self.TeamName = Name
+ 
+  def DefineTeamOrigin(self, Origin):
+    self.TeamOrigin = Origin    
+
+Team1 = Team()
+
+print(Team1.TeamName)
+
+Team1.DefineTeamName("test")
+
+print(Team1.TeamName)
